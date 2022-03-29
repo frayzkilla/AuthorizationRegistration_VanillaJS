8f1d54551d1a7afff5ddb98722fa577d056fbff0
frayzkilla <frayzthamotion@gmail.com>
2022-03-30 01:19:53 +0800

    added all starting animations

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
+const blurDiv = function(){
+    let div = document.querySelector('.login__box');
+    div.classList.add('blur-in');
+}
 
+const unblurDiv = function(){
+    let div = document.querySelector('.login__box');
+    /* div.classList.remove('blur-in'); */
+    div.classList.add('blur-out');
+}
 
 const signIn = function(){
-    header = document.querySelector('.h1');
-    boxes = document.querySelectorAll('.text__box');
-    buttons = document.querySelectorAll('.btn');
+    let header = document.querySelector('.h1');
+    let boxes = document.querySelectorAll('.text__box');
+    let buttons = document.querySelectorAll('.btn');
+
+    boxes.forEach(element => {
+        element.classList.remove('slit-out-reverse');
+    });
+    buttons.forEach(element => {
+        element.classList.remove('fade-out-reverse');
+    });
+    header.classList.remove('slit-out-reverse');
+    
 
     const hideBoxes = function(){
         boxes.forEach(element => {
             element.classList.add('slit-out');
         });
+        header.classList.add('slit-out');
     }
 
     const hideButtons = function(){
@@ -17,9 +36,15 @@ const signIn = function(){
         });
     }
 
-    header.classList.add('slit-out');
+    const showSigned = function(){
+        let signed = document.querySelector('.signedIn');
+        signed.classList.add('fade-out-reverse');
+    }
+
     setTimeout(hideBoxes, 400);
     setTimeout(hideButtons, 800);
+    setTimeout(showSigned, 1200);
 
 }
+
 
