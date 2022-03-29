6121c868b864cc64e1ad73b50f9ddc211dc2b468
frayzkilla <frayzthamotion@gmail.com>
2022-03-30 02:31:35 +0800

    added registration indicator w/out scripts

--- script.js
+++ script.js
@@ -9,6 +9,44 @@ const unblurDiv = function(){
     div.classList.add('blur-out');
 }
 
+const registration = function(){
+    let header = document.querySelector('.h1');
+    let boxes = document.querySelectorAll('.text__box');
+    let buttons = document.querySelectorAll('.btn');
+
+    boxes.forEach(element => {
+        element.classList.remove('slit-out-reverse');
+    });
+    buttons.forEach(element => {
+        element.classList.remove('fade-out-reverse');
+    });
+    header.classList.remove('slit-out-reverse');
+    
+
+    const hideBoxes = function(){
+        boxes.forEach(element => {
+            element.classList.add('slit-out');
+        });
+        header.classList.add('slit-out');
+    }
+
+    const hideButtons = function(){
+        buttons.forEach(element => {
+            element.classList.add('fade-out');
+        });
+    }
+
+    const showRegistration = function(){
+        let reg = document.querySelector('.registration__wrapper');
+        reg.classList.add('fade-out-reverse');
+    }
+
+    setTimeout(hideBoxes, 400);
+    setTimeout(hideButtons, 800);
+    setTimeout(showRegistration, 1200);
+
+}
+
 const signIn = function(){
     let header = document.querySelector('.h1');
     let boxes = document.querySelectorAll('.text__box');
